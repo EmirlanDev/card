fcccaf747ca302b9dd8ebcc1e8e85e70a8ca638b
EmirlanDev <frdevemirlan@gmail.com>
2023-12-07 01:46:16 +0600

    add open addNewCart

---
+++ index.js
@@ -0,0 +1,7 @@
+let add = document.querySelector('.add')
+let addNewCard = document.querySelector('.add-new-cart')
+
+add.addEventListener('click', ()=>{
+    addNewCard.style.display = 'flex'
+})
+
